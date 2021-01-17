27c9792322d7b8756851b219ca25ac25f9728158
mafranks <mafranks@cisco.com>
2021-01-16 20:42:27 -0500

    Removed menu since it was breaking the events. Added welcome message.

--- discord_bot.py
+++ discord_bot.py
@@ -13,6 +13,7 @@ load_dotenv(dotenv_path=Path('.', '.env'))
 # Pull in the credentials
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 GUILD_NAME = os.getenv('GUILD_NAME')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Intents gives the bot permissions
 intents = discord.Intents.all()
@@ -24,54 +25,27 @@ def display_server_information(guild):
     '''Display the server information'''
     # Print the bot and guild information
     print(f'{client.user} is connected to:\n\t{guild.name} (id: {guild.id})')
-    menu(guild)
 
 def display_users(guild):
     '''Display the users on the server'''
     # Find and print all of the guild members
     members = '\n - '.join([member.name for member in guild.members])
-    '''
-    The list comprehension [member.name for member in guild.members] could also be
-    written like this:
-
-    members = []
-    for member in guild.members:
-        members.append('\n - '.join(member.name))
-    '''
     print(f'Guild Members:\n - {members}')
-    menu(guild)
-
-def menu(guild):
-    '''Display the menu'''
-    print(f"\n\n{30 * '-'}")
-    print(f'Please make a selection and {client.user} will comply:')
-    print(30 * '-')
-    print("[1] Display Server Information")
-    print("[2] Display Users")
-    print(f"{30 * '-'}\n\n")
-
-    ## Get input ###
-    choice = input('Enter your numerical choice from above: ')
-
-    ### Convert string to int type ##
-    choice = int(choice)
-
-    ### Take action as per selected menu-option ###
-    if choice == 1:
-        display_server_information(guild)
-    elif choice == 2:
-        display_users(guild)
-    else:    ## default ##
-        print("Invalid number. Select one of the available options.")
 
 @client.event
 async def on_ready():
     '''
-    Define the guild and call the menu when the bot connects to the server (guild)
+    Main function to view options on the CLI
     '''
     # Loop through all the servers and look for one that matches the GUILD_NAME
-    guild = discord.utils.find(lambda guild: guild.name == GUILD_NAME, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD_NAME)
+    display_server_information(guild)
+    display_users(guild)
 
-    menu(guild)
+@client.event
+async def on_member_join(member):
+    '''Welcome new members'''
+    await member.create_dm()
+    await member.dm_channel.send(f"Hello {member.name}, welcome to the server.")
 
 client.run(BOT_TOKEN)
